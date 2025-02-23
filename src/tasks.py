d07ac4a2b733ed38a38f82b1a08bf9d775db6575
Harmehar Kaur <84430398+harmehar-kaur@users.noreply.github.com>
2025-02-23 16:28:11 +0530

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -55,7 +55,7 @@ def command(
         os.mkdir(extract_directory)
 
         command = [
-            "/forensics/7zz", "x",
+            "/forensics/7zip/7zz", "x",
             input_file.get("path"),
             f"-o{extract_directory}",
             "-y"
